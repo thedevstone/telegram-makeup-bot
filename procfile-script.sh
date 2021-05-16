d083b5b2a195c1684a98f69a1498068df515e019
Luca Giulianini <luca.giulianini2@studio.unibo.it>
2021-05-16 23:58:32 +0200

    Dlib

--- procfile-script.sh
+++ procfile-script.sh
@@ -1,3 +1,3 @@
-ENV DEBIAN_FRONTEND=noninteractive
-ENV DEBCONF_NONINTERACTIVE_SEEN=true
+DEBIAN_FRONTEND=noninteractive
+DEBCONF_NONINTERACTIVE_SEEN=true
 RUN apt-get update && apt-get install -y python3-opencv
